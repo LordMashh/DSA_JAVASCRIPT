fc619c663ecac8fbb817800939828e5182dc8a6d
LordMashh <mayankkush0842@gmail.com>
2023-11-22 21:02:47 +0530

    summary

--- leetcode/lowestpossiblevalues.js
+++ leetcode/lowestpossiblevalues.js
@@ -1,3 +1,25 @@
+/* Given an array A of N positive integers and an integer M, your task is to pick M integers from A into an array B such that the function F given below gives the least possible value:
+F(B) Max(B)- Min(B)
+
+Print the lowest possible value of the given function F.
+
+Input format
+The first line contains two positive integers N and M.
+The second line contains N space-separated integers in the array A.
+O<N<= 100
+1<M<=N
+O<A[i]<= 100
+
+Output format
+A single number denoting the lowest value of the given function F
+
+Sample input
+8 5 
+3 4 3 8 1 15 20 3
+
+Sample Outpt
+3 */
+
 function findMinimumF(arr, n, m) {
     // Sort the array in ascending order
     arr.sort((a, b) => a - b);
--- leetcode/phonenumbervalidator.js
+++ leetcode/phonenumbervalidator.js
@@ -1,3 +1,35 @@
+/* Refer the sample input and output to better understand these rules. 
+* The first line of input contains a positive integer N representing the number of phone numbers. The next lines of input contain phone numbers.
+Note: The given strings may contain space(s).
+The input must be considered till the end of the line.
+
+INPUT Sample Input
+10 8147326014
++918147326014
+08147326014
++91 81473 26014
+ 081473 26014
+81473 26014
+8147abcd 123
+814-732-6014
+ 814 7326014
+
+* OUTPUT
+True
+True
+True
+True
+True
+False
+False
+False
+False
+False
+
+Print the validity of each phone number in a separate line - "True" if the phone number is valid, "False" otherwise.
+
+ CONSTRAINTS 1 <= N <= 1000 */
+
 function validatePhoneNumber(phoneNumber) {
     // Remove all spaces from the phone number
     phoneNumber = phoneNumber.replace(" ", "");

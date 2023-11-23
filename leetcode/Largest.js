f3fb70033779f194f143ec950a581f5b4027dd7c
LordMashh <mayankkush0842@gmail.com>
2023-11-23 11:23:14 +0530

    summaries

--- leetcode/Largest.js
+++ leetcode/Largest.js
@@ -16,6 +16,67 @@ Example Output
 
  */
 
+// function largestMinDist(arr, n, k) {
+//     // Sort the array in ascending order
+//     arr.sort((a, b) => a - b);
+  
+//     // Set the low and high values for binary search
+//     let low = 1;
+//     let high = arr[n - 1] - arr[0];
+  
+//     // Perform binary search to find the largest minimum distance
+//     while (low <= high) {
+//       let mid = Math.floor((low + high) / 2);
+  
+//       if (isFeasible(mid, arr, n, k)) {
+//         low = mid + 1;
+//       } else {
+//         high = mid - 1;
+//       }
+//     }
+  
+//     // Return the largest minimum distance
+//     return high;
+//   }
+  
+//   function isFeasible(mid, arr, n, k) {
+//     // Initialize the position of the last placed room
+//     let lastPlacedRoom = arr[0];
+  
+//     // Initialize the count of rooms placed
+//     let roomsPlaced = 1;
+  
+//     // Check if it is possible to place k rooms with minimum distance mid
+//     for (let i = 1; i < n; i++) {
+//       if (arr[i] - lastPlacedRoom >= mid) {
+//         // Place the next room if its distance from the previously placed room
+//         // is greater than or equal to the current mid
+//         lastPlacedRoom = arr[i];
+//         roomsPlaced++;
+  
+//         // Check if all rooms are placed successfully
+//         if (roomsPlaced === k) {
+//           return true;
+//         }
+//       }
+//     }
+  
+//     // If all rooms cannot be placed with minimum distance mid, return false
+//     return false;
+//   }
+  
+//   // Read the input
+//   const input = [5, 3, 1, 2, 4, 8, 9];
+//   const n = input[0];
+//   const k = input[1];
+//   const arr = input.slice(2);
+  
+//   // Find the largest minimum distance
+//   const result = largestMinDist(arr, n, k);
+//   console.log(result);
+  
+
+
 function findLargestMinDistance(N, K, A) {
     // Sort the array A in ascending order
     A.sort((a, b) => a - b);
@@ -57,10 +118,10 @@ function isPossible(minDistance, N, K, A) {
 }
 
 // Example Input
-let input = [1, 2, 4, 8, 9];
-let N = input.length;
-let K = 3;
+let input = "5 3\n1 2 4 8 9";
+let [N, K] = input.split("\n")[0].split(" ").map(Number);
+let A = input.split("\n")[1].split(" ").map(Number);
 
 // Example Output
-let result = findLargestMinDistance(N, K, input);
+let result = findLargestMinDistance(N, K, A);
 console.log(result);
--- leetcode/phonenumbervalidator.js
+++ leetcode/phonenumbervalidator.js
@@ -1,76 +1,103 @@
-/* Refer the sample input and output to better understand these rules. 
-* The first line of input contains a positive integer N representing the number of phone numbers. The next lines of input contain phone numbers.
-Note: The given strings may contain space(s).
-The input must be considered till the end of the line.
+// /* Refer the sample input and output to better understand these rules. 
+// * The first line of input contains a positive integer N representing the number of phone numbers. The next lines of input contain phone numbers.
+// Note: The given strings may contain space(s).
+// The input must be considered till the end of the line.
 
-INPUT Sample Input
-10 8147326014
-+918147326014
-08147326014
-+91 81473 26014
- 081473 26014
-81473 26014
-8147abcd 123
-814-732-6014
- 814 7326014
+// INPUT Sample Input
+// 10 8147326014
+// +918147326014
+// 08147326014
+// +91 81473 26014
+//  081473 26014
+// 81473 26014
+// 8147abcd 123
+// 814-732-6014
+//  814 7326014
 
-* OUTPUT
-True
-True
-True
-True
-True
-False
-False
-False
-False
-False
+// * OUTPUT
+// True
+// True
+// True
+// True
+// True
+// False
+// False
+// False
+// False
+// False
 
-Print the validity of each phone number in a separate line - "True" if the phone number is valid, "False" otherwise.
+// Print the validity of each phone number in a separate line - "True" if the phone number is valid, "False" otherwise.
 
- CONSTRAINTS 1 <= N <= 1000 */
+//  CONSTRAINTS 1 <= N <= 1000 */
+
+// function validatePhoneNumber(phoneNumber) {
+//     // Remove all spaces from the phone number
+//     phoneNumber = phoneNumber.replace(" ", "");
+  
+//     // Check if the phone number starts with a '+' or '0'
+//     if (!phoneNumber.startsWith('+') && !phoneNumber.startsWith('0')) {
+//       return false;
+//     }
+  
+//     // Check if the phone number contains only digits
+//     if (!/^\d+$/.test(phoneNumber)) {
+//       return false;
+//     }
+  
+//     // Check if the phone number has a valid length
+//     if (phoneNumber.length < 10 || phoneNumber.length > 15) {
+//       return false;
+//     }
+  
+//     return true;
+//   }
+  
+//   // Read the input
+//   const input = [
+//     "8147326014",
+//     "+918147326014",
+//     "08147326014",
+//     "+91 81473 26014",
+//     " 081473 26014",
+//     "81473 26014",
+//     "8147abcd 123",
+//     "814-732-6014",
+//     " 814 7326014",
+//   ];
+  
+//   // Validate each phone number
+//   for (const phoneNumber of input) {
+//     if (validatePhoneNumber(phoneNumber)) {
+//       console.log("True");
+//     } else {
+//       console.log("False");
+//     }
+//   }
 
 function validatePhoneNumber(phoneNumber) {
-    // Remove all spaces from the phone number
-    phoneNumber = phoneNumber.replace(" ", "");
-  
-    // Check if the phone number starts with a '+' or '0'
-    if (!phoneNumber.startsWith('+') && !phoneNumber.startsWith('0')) {
-      return false;
-    }
-  
-    // Check if the phone number contains only digits
-    if (!/^\d+$/.test(phoneNumber)) {
-      return false;
-    }
-  
-    // Check if the phone number has a valid length
-    if (phoneNumber.length < 10 || phoneNumber.length > 15) {
-      return false;
-    }
-  
-    return true;
-  }
-  
-  // Read the input
-  const input = [
-    "8147326014",
-    "+918147326014",
-    "08147326014",
-    "+91 81473 26014",
-    " 081473 26014",
-    "81473 26014",
-    "8147abcd 123",
-    "814-732-6014",
-    " 814 7326014",
-  ];
-  
-  // Validate each phone number
-  for (const phoneNumber of input) {
-    if (validatePhoneNumber(phoneNumber)) {
-      console.log("True");
-    } else {
-      console.log("False");
-    }
-  }
+  // Define the regular expression for a valid phone number
+  const phoneRegex = /^(\+?\d{1,4})?[ -]?\d{10}$/;
+
+  // Test the input phone number against the regular expression
+  return phoneRegex.test(phoneNumber);
+}
+
+// Test the provided phone numbers
+const phoneNumbers = [
+  "8147326014",
+  "+918147326014",
+  "08147326014",
+  "+91 81473 26014",
+  " 081473 26014",
+  "81473 26014",
+  "8147abcd 123",
+  "814-732-6014",
+  " 814 7326014"
+];
+
+// Validate and print the result for each phone number
+phoneNumbers.forEach(number => {
+  console.log(`${number}: ${validatePhoneNumber(number) ? 'Valid' : 'Invalid'}`);
+});
+
   
